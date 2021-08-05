f68b8fd8c6bf8f691e73c558e127bc6f77715e48
Gautami09 <88287813+Gautami09@users.noreply.github.com>
2021-08-05 23:30:59 +0530

    Update Gautami_perl.pl

--- Gautami_perl.pl
+++ Gautami_perl.pl
@@ -1,1 +1,18 @@
-print "Name: Gautami Patki\nEmail ID: gautamipatki\@gmail.com\nSlack username: \@Gautami\nBiostack: Genomics\n";
+$Slack_username = "\@Gautami";  
+$Twitter_username = "\@GPATKI_"; 
+$a = length($Slack_username);
+
+$HAM = 0 ;
+for ($i=1; $i<=$a; $i++)
+ {
+     $c = chop($Slack_username);
+     $d = chop($Twitter_username);
+     
+     if (($c cmp $d) !=0) 
+  { 
+      $HAM = $HAM + 1;
+  }
+}
+
+print "Name: Gautami Patki\nEmail ID: gautamipatki\@gmail.com\nSlack username: \@Gautami\nBiostack: Genomics\nTwitter username: \@GPATKI_\nHamming Distance= $HAM\n";
+
