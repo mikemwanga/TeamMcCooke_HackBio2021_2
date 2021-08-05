2660750928b43e98b3a8485a687330382abd5168
Lu-17 <70852842+Lu-17@users.noreply.github.com>
2021-08-05 13:13:46 -0500

    Update Luisa.R

--- Luisa.R
+++ Luisa.R
@@ -1,6 +1,44 @@
-#Luisa Personal information
+## Script to print Personal Information in R
 
-print("Name: Luisa Criollo")
-print("E-mail: luisacriollo17@gmail.com")
-print("Slack username: @Luisa")
-print("Biostack: Drug development")
+Name <- 'Luisa Criollo'
+Email <- 'luisacriollo17@gmail.com'
+Slack_username <- '@LuisaC'
+Biostack <- 'Drug development'
+Twitter_username <- '@Lu_317'
+
+
+##Function to calculate
+# Hamming distance
+
+HammingDist <- function(Slack_username, Twitter_username){
+  
+  s <- as.character(Slack_username)
+  t <- as.character(Twitter_username)
+  
+  length_s <- nchar(s) #Returns the length of the string
+  length_t <- nchar(t)
+  i<-0
+  
+  s_sub <- c()
+  for (i in 1:length_s){
+    s_sub[i] = substr(s, start=i, stop=i)
+  }
+  
+  t_sub <- c()
+  for (i in 1:length_t){
+    t_sub[i] = substr(t, start=i, stop=i)
+  }
+  
+  if (length_s == length_t)  {
+    
+    
+    return(sum(s_sub != t_sub)) #Compare the strings and return the Hamming Distance
+    
+    
+  }
+}
+  
+  HammingDistance <- HammingDist(Slack_username, Twitter_username) #Save the Hamming distance's value
+  
+  cat(paste(Name,Email, Slack_username,Biostack,Twitter_username,HammingDistance, sep= "\n")) #Print the information
+ 
