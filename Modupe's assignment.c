76cbb15db168261b1655f2b9bccfdb834778bbea
Anthony Modupe <modupemercyanthony@gmail.com>
2021-08-06 09:10:06 +0100

    Delete Modupe's assignment.c
    I deleted my previous assignment to upload the new one

--- Modupe's assignment.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>>
-
-int main()
-{
-	printf("\n NAME:Anthony Modupe");
-	printf("\n EMAIL:modupemercyanthony@gmail.com");
-	printf("\n Slack USERNAME:@modupe");
-	printf("\n BIOSTACK:Data Analysis");
-}
