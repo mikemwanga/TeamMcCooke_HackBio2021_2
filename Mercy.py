1cdd1873545ae8ad4737a20e1d879554cac214d2
Anthony Modupe <modupemercyanthony@gmail.com>
2021-08-06 14:20:26 +0100

    Mercy.py
    This is my new assignment

---
+++ Mercy.py
@@ -0,0 +1,14 @@
+print("Anthony Modupe Mercy")
+print("modupemercyanthony@gmail.com")
+print("@Mercy")
+print("Data analysis")
+print("Dupe_ ")
+def hamming_distance(string1, string2): 
+    distance = 0
+    L = len(string1)
+    for i in range(L):
+        if string1[i] != string2[i]:
+            distance += 1
+    return distance
+answer = hamming_distance("Mercy", "Dupe_")
+print(answer)
