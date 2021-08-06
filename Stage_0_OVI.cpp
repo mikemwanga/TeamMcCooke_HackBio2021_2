da0c08590ea78970a3540ebb524f61b2cf3b4096
Trisad <88211669+Trisad@users.noreply.github.com>
2021-08-06 23:04:20 +0600

    Update Stage_0_OVI.cpp

--- Stage_0_OVI.cpp
+++ Stage_0_OVI.cpp
@@ -2,42 +2,27 @@
 ///------Trisad Arman Ovi------
 ///------HackBio Bioinformatics Internship2021------
 ///------TeamMcCooke_HackBio2021------
-///------programming languages C++ ------
 
-#include<bits/stdc++.h>
+#include <iostream>
+
 using namespace std;
-int hammingDist(char *str1, char *str2)
-{
-	int i = 0, count = 0;
-	while (str1[i] != '\0')
-	{
-		if (str1[i] != str2[i])
-			count++;
-		i++;
-	}
-	return count;
-}
 
 int main()
 {
-	
-	cout<<"My Name:TRISAD ARMAN OVI"<<endl;
-
-    cout<<"My E-Mail:ta872068@student.nitw.ac.in"<<endl;
-
-    cout<<"My Language:C++"<<endl;
-
-    cout<<"My Biostack:Drug Development"<<endl;
-
-    cout<<"My Username_Slack:@OVI "<<endl;
     
-    cout<<"My Twitter_Handle:@ovi_trisad"<<endl;
-    
-    char str1[] = "@OVI";
-    
-	char str2[] = "@ovi_trisad";
-    
-    cout << hammingDist (str1, str2);
+    cout<<"Name:TRISAD ARMAN OVI"<<endl;
 
-	return 0;
+    cout<<"email:ta872068@student.nitw.ac.in"<<endl;
+    
+    cout<<"@slack username_:@OVI "<<endl;
+    
+    cout<<"Biostack:Drug Development"<<endl;
+
+    cout<<"Twitter username:@ovi_trisad"<<endl;
+    
+    cout<<"Hamming distance:'3'";
+    
+    
+    return 0;
 }
+    
