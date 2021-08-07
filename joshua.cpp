b8907b2cef0f6619078c235cdeecd524635376c9
Joshua1w <88293828+Joshua1w@users.noreply.github.com>
2021-08-07 09:28:00 +0100

    Update joshua.cpp

--- joshua.cpp
+++ joshua.cpp
@@ -1,3 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int hammingDist(char *str1, char *str2)
+     {
+	int i = 0, count = 0;
+	while (str1[i] != '\0')
+	{
+		if (str1[i] != str2[i])
+			count++;
+		i++;
+	}
+	return count;
+   }
+
 #include <iostream>
 
 using namespace std;
