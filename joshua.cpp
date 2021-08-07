f5afb6a51716ae6d4e87c26d2624cf741f95c002
Joshua1w <88293828+Joshua1w@users.noreply.github.com>
2021-08-07 08:59:58 +0100

    Add files via upload

---
+++ joshua.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+     /* Name */
+    cout << "Fakorede Joshua Adeleye" << endl;
+
+    /* Email*/
+      cout << "Fakorede19@gmail.com" << endl;
+
+    /*Slack_Username*/
+        cout << "Joshua0" << endl;
+
+    /* My biostack */
+      cout << "@Drug Development " << endl;
+
+    /* Twitter_Handle*/
+      cout << "@leyeleye0" << endl;
+
+
+
+    return 0;
+}
+
+
