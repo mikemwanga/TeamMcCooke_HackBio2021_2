6bf93f32cc66a43a2002cecd4d9b9b1e96077a73
Joshua1w <88293828+Joshua1w@users.noreply.github.com>
2021-08-07 10:36:34 +0100

    Add files via upload

---
+++ JOSHUA (2).cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int hammingDist(char *str1, char*str2)
+{
+    int i = 0, count = 0;
+   while (str1[i] != '\0')
+    {
+        if (str1[i] !=str2[i])
+                count++;
+
+            i++;
+    }
+
+    return count;
+}
+int main()
+{
+     /* Name */
+    cout << "Fakorede Joshua Adeleye" << endl;
+
+    /* Email*/
+      cout << "Fakorede19@gmail.com" << endl;
+
+    /*Slack_Username*/
+        cout << "@Joshua0" << endl;
+
+    /* My biostack */
+      cout << "Drug Development " << endl;
+
+    /* Twitter_Handle*/
+      cout << "@leyeleye0" << endl;
+
+      /* hamming distance*/
+       char str1[] ="@joshua0";
+
+        char str2[] = "@leyeleye0";
+
+
+
+
+    return 0;
+}
+
+
+
